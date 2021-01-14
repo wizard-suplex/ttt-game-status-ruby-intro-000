027ac03bb42e09a990bc162743d7e90b69c6a81b
Alan Cooke <atc4@rice.edu>
2021-01-14 20:02:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 def won?(board)
  WIN_COMBINATIONS.detect do |win|
    board[win[0]] == board[win[1]] &&
-   board[win[1]] == board[win[2]] &&
+   board[win[0]] == board[win[2]] &&
    position_taken?(board, win[0])
  end
 end
